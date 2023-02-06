822e989e9a6945987f7a60c631c07c68fa0ed7ad
lukas-jj <lukas.jury13@gmail.com>
2023-02-06 17:28:12 +1300

    update index

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import ChatGPT from './ChatGPT';
 import Key from './Key'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
     <Key />
   </React.StrictMode>
 );
