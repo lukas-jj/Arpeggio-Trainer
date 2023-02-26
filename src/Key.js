8ad9cdc848c61f9d2eb81a2023ca25012aef3b7c
lukas-jj <lukas.jury13@gmail.com>
2023-02-26 13:54:32 +1300

    put string and modes object into seperate component

--- src/Key.js
+++ src/Key.js
@@ -1,81 +1,122 @@
-import React, { useState, useEffect, useMemo } from 'react';
-import Guitar from 'react-guitar'
-import { standard } from 'react-guitar-tunings'
-import useSound from 'react-guitar-sound'
-import {modes, noteNamesA} from './MusicData.js';
+import React, { useState, useEffect, useMemo } from "react";
+import Guitar from "react-guitar";
+import { standard } from "react-guitar-tunings";
+import useSound from "react-guitar-sound";
+import { modes, noteNamesA, noteNamesB, emptyFretboard, fretBoardLength } from "./MusicData.js";
 const AudioContext = window.AudioContext || window.webkitAudioContext;
 
-console.log(modes)
-
+console.log(modes);
 
 const ArpeggioPlayer = () => {
-
-  const [mode, setMode] = useState('ionian');
+  const [mode, setMode] = useState("ionian");
   const [isPlaying, setIsPlaying] = useState(false);
-  const [noteName, setNoteName] = useState('');
-  const [rootNote, setRootNote] = useState(57)
-  const [fretPosition, setFretPosition] = useState([-1, -1, -1, -1, -1, -1])
-  const strings = useMemo(() => [-1, -1, -1, -1, -1, -1], [])
-  const { play, strum } = useSound({ fretting: strings, tuning: standard })
-
-  const keyNames = Object.keys(modes).map(m => (
-    <option key={m} value={m}>{m}</option>
-  ))
+  const [noteName, setNoteName] = useState("");
+  const [rootNote, setRootNote] = useState(57);
+  const [fretPosition, setFretPosition] = useState(emptyFretboard);
+  const strings = useMemo(() => emptyFretboard, []);
+  const [fretboardAmount, setFretboardAmount] = useState(fretBoardLength);
+  const { play, strum } = useSound({ fretting: strings, tuning: standard });
+  const { string, setString } = useState('A')
 
 
-  useEffect(() => {    
+
+  const keyNames = Object.keys(modes).map((m) => (
+    <option key={m} value={m}>
+      {m}
+    </option>
+  ));
+
+  function getMidiValueByFretPosition(num) {
+    // iterate through the noteNames object
+    for (let note in noteNamesA) {
+      // if the note's fretPosition is equal to the number passed in, return the midi value
+      if (noteNamesA[note].fretPosition[4] === + num) return noteNamesA[note].midi;
+
+    }
+    // if no match is found, return null
+    return null;
+  }
+
+  useEffect(() => {
     if (!isPlaying) return;
-    const context = new AudioContext()
+    const context = new AudioContext();
     const scale = modes[mode];
-    
+
     let current = 0;
     let oscillator = null;
-    const play = () => {    
-    let note = rootNote + scale[current];
-
-      if (note > 69) {
+    const play = () => {
+      let note = rootNote + scale[current];
+      let topMidi = getMidiValueByFretPosition(fretboardAmount);
+      console.log(topMidi, note, "topMidi, note")
+      if (note > topMidi) {
+        console.log(topMidi)
         note = note - 12;
       }
 
-      setFretPosition(noteNamesA[Object.keys(noteNamesA).find(key => noteNamesA[key].midi === note)].fretPosition)
-      setNoteName(Object.keys(noteNamesA).find(key => noteNamesA[key].midi === note))    
+      setFretPosition(
+        noteNamesA[
+          Object.keys(noteNamesA).find((key) => noteNamesA[key].midi === note)
+        ].fretPosition
+      );
+      setNoteName(
+        Object.keys(noteNamesA).find((key) => noteNamesA[key].midi === note)
+      );
 
       oscillator = context.createOscillator();
       oscillator.frequency.value = 440 * Math.pow(2, (note - 69) / 12);
       oscillator.connect(context.destination);
       oscillator.start();
       oscillator.stop(context.currentTime + 0.5);
-      
+
       current++;
-      
-      if(current < scale.length) 
-        oscillator.addEventListener('ended', play)
+
+      if (current < scale.length) oscillator.addEventListener("ended", play);
     };
-    
+
     play();
-    
+
     return () => {
-      oscillator.removeEventListener('ended', play);
+      oscillator.removeEventListener("ended", play);
       oscillator = null;
       context.close();
     };
-  }, [mode, isPlaying]);  
+  }, [mode, isPlaying]);
 
   return (
+
     <div>
-      <select value={mode} onChange={e => setMode(e.target.value)}>
+      <label for="fretboard-amount">Fretboard Amount:</label>
+      <select id="fretboard-amount" onChange={(e) => setFretboardAmount(e.target.value)} >
+        <option value="12">12</option>
+        <option value="13">13</option>
+        <option value="14">14</option>
+        <option value="15">15</option>
+        <option value="16">16</option>
+        <option value="17">17</option>
+        <option value="18">18</option>
+        <option value="19">19</option>
+        <option value="20">20</option>
+        <option value="21">21</option>
+        <option value="22">22</option>
+      </select>
+      <select value={mode} onChange={(e) => setMode(e.target.value)}>
         {keyNames}
       </select>
-
-      <select value={noteNamesA[rootNote]} onChange={e => setRootNote(noteNamesA[e.target.value].midi)}>
-        {Object.keys(noteNamesA).map(note => (
-          <option key={note} value={note}>Note: {note}</option>
+      f
+      <select
+        value={noteNamesA[rootNote]}
+        onChange={(e) => setRootNote(noteNamesA[e.target.value].midi)}
+      >
+        {Object.keys(noteNamesA).map((note) => (
+          <option key={note} value={note}>
+            Note: {note}
+          </option>
         ))}
       </select>
       <button onClick={() => setIsPlaying(!isPlaying)}>
-        {isPlaying ? 'Stop' : 'Start'}
+        {isPlaying ? "Stop" : "Start"}
       </button>
-      <Guitar id ="Guitar"strings={fretPosition} onPlay={play} />
+      <Guitar className="Guitar" strings={fretPosition} onPlay={play} />
       <h1>{noteName}</h1>
     </div>
   );
--- src/MusicData.js
+++ src/MusicData.js
@@ -1,139 +1,216 @@
 // Parent Component
-import React from 'react';
+// Fretboard setup
+export const emptyFretboard = [-1, -1, -1, -1, -1, -1]
 
-   export const modes = {
-    'ionian': [0, 2, 4, 5, 7, 9, 11, 12],
-    'dorian': [0, 2, 3, 5, 7, 9, 10, 12],
-    'phrygian': [0, 1, 3, 5, 7, 8, 10, 12],
-    'lydian': [0, 2, 4, 6, 7, 9, 11, 12],
-    'mixolydian': [0, 2, 4, 5, 7, 9, 10, 12],
-    'aeolian': [0, 2, 3, 5, 7, 8, 10, 12],
-    'locrian': [0, 1, 3, 5, 6, 8, 10, 12],
-  };
-  
- export  const noteNamesA = {
-    'A': {
-      midi: 57,
-      fretPosition: [-1, -1, -1, -1, 0, -1]
-      },
-    'A#': {
-      midi: 58,
-      fretPosition: [-1, -1, -1, -1, 1, -1]
-      },
-    'B': {
-      midi: 59,
-      fretPosition: [-1, -1, -1, -1, 2, -1]
-      },
-    'C': {
+export const fretBoardLength = 12;
+
+// Notes
+export const noteNamesA = {
+  'A': {
+    midi: 57,
+    fretPosition: [-1, -1, -1, -1, 0, -1]
+  },
+  'A#': {
+    midi: 58,
+    fretPosition: [-1, -1, -1, -1, 1, -1]
+  },
+  'B': {
+    midi: 59,
+    fretPosition: [-1, -1, -1, -1, 2, -1]
+  },
+  'C': {
     midi: 60,
     fretPosition: [-1, -1, -1, -1, 3, -1]
-    },
-    'C#': {
+  },
+  'C#': {
     midi: 61,
     fretPosition: [-1, -1, -1, -1, 4, -1]
-    },
-    'D': {
+  },
+  'D': {
     midi: 62,
     fretPosition: [-1, -1, -1, -1, 5, -1]
-    },
-    'D#': {
+  },
+  'D#': {
     midi: 63,
     fretPosition: [-1, -1, -1, -1, 6, -1]
-    },
-    'E': {    
+  },
+  'E': {
     midi: 64,
     fretPosition: [-1, -1, -1, -1, 7, -1]
-    },
-    'F': {
+  },
+  'F': {
     midi: 65,
     fretPosition: [-1, -1, -1, -1, 8, -1]
-    },
-    'F#': {
+  },
+  'F#': {
     midi: 66,
     fretPosition: [-1, -1, -1, -1, 9, -1]
-    },
-    'G': {
+  },
+  'G': {
     midi: 67,
     fretPosition: [-1, -1, -1, -1, 10, -1]
-    },
-    'G#': {
+  },
+  'G#': {
     midi: 68,
     fretPosition: [-1, -1, -1, -1, 11, -1]
-    },
-    'A2': {
-  
-      midi: 69,
-      fretPosition: [-1, -1, -1, -1, 12, -1]
-      },
-    };
-  
-    // const noteNamesB = {
-    //   'A': {
-    //     string: B,
-    //     midi: 57,
-    //     fretPosition: [-1, -1, -1, -1, 0, -1]
-    //     },
-    //   'A#': {
-    //     string: B,
-    //     midi: 58,
-    //     fretPosition: [-1, -1, -1, -1, 1, -1]
-    //     },
-    //   'B': {
-    //     string: B,
-    //     midi: 59,
-    //     fretPosition: [-1, -1, -1, -1, 2, -1]
-    //     },
-    //   'C': {
-    //     string: B,
-    //   midi: 60,
-    //   fretPosition: [-1, -1, -1, -1, 3, -1]
-    //   },
-    //   'C#': {
-    //     string: B,
-    //   midi: 61,
-    //   fretPosition: [-1, -1, -1, -1, 4, -1]
-    //   },
-    //   'D': {
-    //   midi: 62,
-    //   fretPosition: [-1, -1, -1, -1, 5, -1]
-    //   },
-    //   'D#': {
-    //   string: B,
-    //   midi: 63,
-    //   fretPosition: [-1, -1, -1, -1, 6, -1]
-    //   },
-    //   'E': {    
-    //   string: B,
-    //   midi: 64,
-    //   fretPosition: [-1, -1, -1, -1, 7, -1]
-    //   },
-    //   'F': {
-    //   string: B,
-    //   midi: 65,
-    //   fretPosition: [-1, -1, -1, -1, 8, -1]
-    //   },
-    //   'F#': {
-    //   string: B,
-    //   midi: 66,
-    //   fretPosition: [-1, -1, -1, -1, 9, -1]
-    //   },
-    //   'G': {
-    //   string: B,
-    //   midi: 67,
-    //   fretPosition: [-1, -1, -1, -1, 10, -1]
-    //   },
-    //   'G#': {
-    //   string: B,
-    //   midi: 68,
-    //   fretPosition: [-1, -1, -1, -1, 11, -1]
-    //   },
-    //   'A2': {
-    //     string: B,
-    //     midi: 69,
-    //     fretPosition: [-1, -1, -1, -1, 12, -1]
-    //     },
-    //   };
-  
+  },
+  'A2': {
+    midi: 69,
+    fretPosition: [-1, -1, -1, -1, 12, -1]
+  },
+  'A#2': {
+    midi: 70,
+    fretPosition: [-1, -1, -1, -1, 13, -1]
+  },
+  'B2': {
+    midi: 71,
+    fretPosition: [-1, -1, -1, -1, 14, -1]
+  },
+  'C2': {
+    midi: 72,
+    fretPosition: [-1, -1, -1, -1, 15, -1]
+  },
+  'C#2': {
+    midi: 73,
+    fretPosition: [-1, -1, -1, -1, 16, -1]
+  },
+  'D2': {
+    midi: 74,
+    fretPosition: [-1, -1, -1, -1, 17, -1]
+  },
+  'D#2': {
+    midi: 75,
+    fretPosition: [-1, -1, -1, -1, 18, -1]
+  },
+  'E2': {
+    midi: 76,
+    fretPosition: [-1, -1, -1, -1, 19, -1]
+  },
+  'F2': {
+    midi: 77,
+    fretPosition: [-1, -1, -1, -1, 20, -1]
+  },
+  'F#2': {
+    midi: 78,
+    fretPosition: [-1, -1, -1, -1, 21, -1]
+  },
+  'G2': {
+    midi: 79,
+    fretPosition: [-1, -1, -1, -1, 22, -1]
+  },
+  'G#2': {
+    midi: 80,
+    fretPosition: [-1, -1, -1, -1, 23, -1]
+  },
+  'A3': {
+    midi: 81,
+    fretPosition: [-1, -1, -1, -1, 24, -1]
+  }
+};
 
-    // export default {MusicData};
-    
+const noteNamesB = {
+  'A': {
+    midi: 57,
+    fretPosition: [-1, -1, -1, -1, 0, -1]
+  },
+  'A#': {
+    midi: 58,
+    fretPosition: [-1, -1, -1, -1, 1, -1]
+  },
+  'B': {
+    midi: 59,
+    fretPosition: [-1, -1, -1, -1, 2, -1]
+  },
+  'C': {
+    midi: 60,
+    fretPosition: [-1, -1, -1, -1, 3, -1]
+  },
+  'C#': {
+    midi: 61,
+    fretPosition: [-1, -1, -1, -1, 4, -1]
+  },
+  'D': {
+    midi: 62,
+    fretPosition: [-1, -1, -1, -1, 5, -1]
+  },
+  'D#': {
+    midi: 63,
+    fretPosition: [-1, -1, -1, -1, 6, -1]
+  },
+  'E': {
+    midi: 64,
+    fretPosition: [-1, -1, -1, -1, 7, -1]
+  },
+  'F': {
+    midi: 65,
+    fretPosition: [-1, -1, -1, -1, 8, -1]
+  },
+  'F#': {
+    midi: 66,
+    fretPosition: [-1, -1, -1, -1, 9, -1]
+  },
+  'G': {
+    midi: 67,
+    fretPosition: [-1, -1, -1, -1, 10, -1]
+  },
+  'G#': {
+    midi: 68,
+    fretPosition: [-1, -1, -1, -1, 11, -1]
+  },
+  'A2': {
+    midi: 69,
+    fretPosition: [-1, -1, -1, -1, 12, -1]
+  },
+  'B2': {
+midi: 70,
+fretPosition: [-1, -1, -1, -1, 13, -1]
+  },
+  'C2': {
+midi: 71,
+fretPosition: [-1, -1, -1, -1, 14, -1]
+  },
+  'C#2': {
+midi: 72,
+fretPosition: [-1, -1, -1, -1, 15, -1]
+  },
+  'D2': {
+midi: 73,
+fretPosition: [-1, -1, -1, -1, 16, -1]
+  },
+  'D#2': {
+midi: 74,
+fretPosition: [-1, -1, -1, -1, 17, -1]
+  },
+  'E2': {
+midi: 75,
+fretPosition: [-1, -1, -1, -1, 18, -1]
+  },
+  'F2': {
+midi: 76,
+fretPosition: [-1, -1, -1, -1, 19, -1]
+  },
+  'F#2': {
+midi: 77,
+fretPosition: [-1, -1, -1, -1, 20, -1]
+  },
+  'G2': {
+midi: 78,
+fretPosition: [-1, -1, -1, -1, 21, -1]
+  },
+  'G#2': {
+midi: 79,
+fretPosition: [-1, -1, -1, -1, 22, -1]
+  }
+};
+
+//Theory
+export const modes = {
+  'ionian': [0, 2, 4, 5, 7, 9, 11, 12],
+  'dorian': [0, 2, 3, 5, 7, 9, 10, 12],
+  'phrygian': [0, 1, 3, 5, 7, 8, 10, 12],
+  'lydian': [0, 2, 4, 6, 7, 9, 11, 12],
+  'mixolydian': [0, 2, 4, 5, 7, 9, 10, 12],
+  'aeolian': [0, 2, 3, 5, 7, 8, 10, 12],
+  'locrian': [0, 1, 3, 5, 6, 8, 10, 12],
+};
+
